7d3ba18374a5bde975b1a81a37af3f74cafb9e06
Mariam-Alboziri <102658576+Mariam-Alboziri@users.noreply.github.com>
2022-06-29 10:27:19 +0300

    add public controller

---
+++ app/Http/Controllers/Public/MedicineController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\Public;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Medicine;
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+
+class MedicineController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+       // $medicines=Medicine::orWhere('name','like',"%$request->q%")
+       // ->orWhere('brand','like',"%$request->q%");
+$query=Medicine::query();
+if ($request->filled('category')){
+
+
+    $query->where('category_id',$request->category);
+}
+if ($request->filled('q')){
+
+    $query->where(function($q) use($request){
+$q->where('name','like', "%$request->q%" );
+    } );
+}
+
+
+
+
+        $medicines=$query->get();
+        $categories=Category::has('medicines')->get();
+        return view ('public.medicines.index',compact('medicines','categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Medicine  $medicine
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Medicine $medicine)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Medicine  $medicine
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Medicine $medicine)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Medicine  $medicine
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Medicine $medicine)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Medicine  $medicine
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Medicine $medicine)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Admin\MedicineController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MessageController;
+use App\Http\Controllers\Public\MedicineController as PublicMedicineController;
 use App\Models\Medicine;
 //use App\Http\Controllers\Public\CarController as PublicCarController;
 //use App\Http\Controllers\Public\CategoryController as PublicCategoryController;
@@ -30,20 +31,24 @@ Route::get('/',[HomeController::class,'welcome'])->name('home');
 Route::view('/contact-us','pages.contact')->name('contact');
 
 Route::post('/contact-us',[MessageController::class,'store'])->name('messages.store');
+Route::resource('medicines',PublicMedicineController::class);
 
+Route::name('admin.')->group(function(){
 Route::prefix('admin')->group(function () {
 
 Route::get('messages', [MessageController::class,'index'])->name('messages.index');
 Route::get('messages/{message}', [MessageController::class,'show'])->name('messages.show');
 
-Route::get('medicines/create',[MedicineController::class,'create'])->name('medicines.create');
-Route::get('medicines',[MedicineController::class,'show'])->name('medicines.show');
-Route::get('medicines', [MedicineController::class,'index'])->name('medicines.index');
-Route::post('medicines',[MedicineController::class,'store'])->name('medicines.store');
-Route::get('medicines/{medicine}/edit',[MedicineController::class,'edit'])->name('medicines.edit');
-Route::put('medicines/{medicine}',[MedicineController::class,'update'])->name('medicines.update');
-Route::delete('medicines/{medicine}',[MedicineController::class,'destroy'])->name('medicines.destroy');
+// Route::get('medicines/create',[MedicineController::class,'create'])->name('medicines.create');
+// Route::get('medicines',[MedicineController::class,'show'])->name('medicines.show');
+// Route::get('medicines', [MedicineController::class,'index'])->name('medicines.index');
+// Route::post('medicines',[MedicineController::class,'store'])->name('medicines.store');
+// Route::get('medicines/{medicine}/edit',[MedicineController::class,'edit'])->name('medicines.edit');
+// Route::put('medicines/{medicine}',[MedicineController::class,'update'])->name('medicines.update');
+// Route::delete('medicines/{medicine}',[MedicineController::class,'destroy'])->name('medicines.destroy');
 
+Route::resource('medicines' , MedicineController::class);
 Route::resource('categories',CategoryController::class);
+});
 
 });
