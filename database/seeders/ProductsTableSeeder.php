c0bb0c2d4eccce9804cb94a122018213b3139299
Mariam-Alboziri <102658576+Mariam-Alboziri@users.noreply.github.com>
2022-07-14 22:54:24 +0300

    create products seeder

---
+++ database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Product::create([
+            'name'=>'Hyloram',
+            'slug'=>'Sodium',
+            'details'=>'sodium 0.4,rx-only',
+            'price' =>'3500',
+            'description'=>'Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Alias culpa repellendus eveniet, ipsam esse adipisci nihil, doloribus enim qui recusandae
+            assumenda suscipit velit voluptatum dolor asperiores? Similique cumque nostrum ratione?',
+
+        ]);
+
+        Product::create([
+            'name'=>'Hyloram',
+            'slug'=>'Sodium',
+            'details'=>'sodium 0.4,rx-only',
+            'price' =>'3500',
+            'description'=>'Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Alias culpa repellendus eveniet, ipsam esse adipisci nihil, doloribus enim qui recusandae
+            assumenda suscipit velit voluptatum dolor asperiores? Similique cumque nostrum ratione?',
+
+        ]);
+
+        Product::create([
+            'name'=>'Hyloram',
+            'slug'=>'Sodium',
+            'details'=>'sodium 0.4,rx-only',
+            'price' =>'3500',
+            'description'=>'Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Alias culpa repellendus eveniet, ipsam esse adipisci nihil, doloribus enim qui recusandae
+            assumenda suscipit velit voluptatum dolor asperiores? Similique cumque nostrum ratione?',
+
+        ]);
+
+        Product::create([
+            'name'=>'Hyloram',
+            'slug'=>'Sodium',
+            'details'=>'sodium 0.4,rx-only',
+            'price' =>'3500',
+            'description'=>'Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Alias culpa repellendus eveniet, ipsam esse adipisci nihil, doloribus enim qui recusandae
+            assumenda suscipit velit voluptatum dolor asperiores? Similique cumque nostrum ratione?',
+
+        ]);
+
+        Product::create([
+            'name'=>'Hyloram',
+            'slug'=>'Sodium',
+            'details'=>'sodium 0.4,rx-only',
+            'price' =>'3500',
+            'description'=>'Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Alias culpa repellendus eveniet, ipsam esse adipisci nihil, doloribus enim qui recusandae
+            assumenda suscipit velit voluptatum dolor asperiores? Similique cumque nostrum ratione?',
+
+        ]);
+
+        Product::create([
+            'name'=>'Hyloram',
+            'slug'=>'Sodium',
+            'details'=>'sodium 0.4,rx-only',
+            'price' =>'3500',
+            'description'=>'Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Alias culpa repellendus eveniet, ipsam esse adipisci nihil, doloribus enim qui recusandae
+            assumenda suscipit velit voluptatum dolor asperiores? Similique cumque nostrum ratione?',
+
+        ]);
+
+        Product::create([
+            'name'=>'Hyloram',
+            'slug'=>'Sodium',
+            'details'=>'sodium 0.4,rx-only',
+            'price' =>'3500',
+            'description'=>'Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Alias culpa repellendus eveniet, ipsam esse adipisci nihil, doloribus enim qui recusandae
+            assumenda suscipit velit voluptatum dolor asperiores? Similique cumque nostrum ratione?',
+
+        ]);
+
+        Product::create([
+            'name'=>'Hyloram',
+            'slug'=>'Sodium',
+            'details'=>'sodium 0.4,rx-only',
+            'price' =>'3500',
+            'description'=>'Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Alias culpa repellendus eveniet, ipsam esse adipisci nihil, doloribus enim qui recusandae
+            assumenda suscipit velit voluptatum dolor asperiores? Similique cumque nostrum ratione?',
+
+        ]);
+
+        Product::create([
+            'name'=>'Hyloram',
+            'slug'=>'Sodium',
+            'details'=>'sodium 0.4,rx-only',
+            'price' =>'3500',
+            'description'=>'Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Alias culpa repellendus eveniet, ipsam esse adipisci nihil, doloribus enim qui recusandae
+            assumenda suscipit velit voluptatum dolor asperiores? Similique cumque nostrum ratione?',
+
+        ]);
+
+        Product::create([
+            'name'=>'Hyloram',
+            'slug'=>'Sodium',
+            'details'=>'sodium 0.4,rx-only',
+            'price' =>'3500',
+            'description'=>'Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Alias culpa repellendus eveniet, ipsam esse adipisci nihil, doloribus enim qui recusandae
+            assumenda suscipit velit voluptatum dolor asperiores? Similique cumque nostrum ratione?',
+
+        ]);
+
+        Product::create([
+            'name'=>'Hyloram',
+            'slug'=>'Sodium',
+            'details'=>'sodium 0.4,rx-only',
+            'price' =>'3500',
+            'description'=>'Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Alias culpa repellendus eveniet, ipsam esse adipisci nihil, doloribus enim qui recusandae
+            assumenda suscipit velit voluptatum dolor asperiores? Similique cumque nostrum ratione?',
+
+        ]);
+
+        Product::create([
+            'name'=>'Hyloram',
+            'slug'=>'Sodium',
+            'details'=>'sodium 0.4,rx-only',
+            'price' =>'3500',
+            'description'=>'Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Alias culpa repellendus eveniet, ipsam esse adipisci nihil, doloribus enim qui recusandae
+            assumenda suscipit velit voluptatum dolor asperiores? Similique cumque nostrum ratione?',
+
+        ]);
+
+        Product::create([
+            'name'=>'Hyloram',
+            'slug'=>'Sodium',
+            'details'=>'sodium 0.4,rx-only',
+            'price' =>'3500',
+            'description'=>'Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Alias culpa repellendus eveniet, ipsam esse adipisci nihil, doloribus enim qui recusandae
+            assumenda suscipit velit voluptatum dolor asperiores? Similique cumque nostrum ratione?',
+
+        ]);
+
+        Product::create([
+            'name'=>'Hyloram',
+            'slug'=>'Sodium',
+            'details'=>'sodium 0.4,rx-only',
+            'price' =>'3500',
+            'description'=>'Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Alias culpa repellendus eveniet, ipsam esse adipisci nihil, doloribus enim qui recusandae
+            assumenda suscipit velit voluptatum dolor asperiores? Similique cumque nostrum ratione?',
+
+        ]);
+
+        Product::create([
+            'name'=>'Hyloram',
+            'slug'=>'Sodium',
+            'details'=>'sodium 0.4,rx-only',
+            'price' =>'3500',
+            'description'=>'Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Alias culpa repellendus eveniet, ipsam esse adipisci nihil, doloribus enim qui recusandae
+            assumenda suscipit velit voluptatum dolor asperiores? Similique cumque nostrum ratione?',
+
+        ]);
+
+    }
+}
