983e5b1c01d383e1b12e0dddb1c0b8e5189f2676
Mariam-Alboziri <102658576+Mariam-Alboziri@users.noreply.github.com>
2022-08-12 19:53:40 +0300

    add company to siedbar

--- resources/views/layouts/inc/admin/sidebar.blade.php
+++ resources/views/layouts/inc/admin/sidebar.blade.php
@@ -23,6 +23,21 @@
       <li class="nav-item">
         <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
           <i class="mdi mdi-circle-outline menu-icon"></i>
+          <span class="menu-title">Company</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="ui-basic">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="{{ route('admin.companies.create') }}">Add Company</a></li>
+            <li class="nav-item"> <a class="nav-link" href="{{ route ('admin.companies.index') }}">Show Company</a></li>
+          </ul>
+        </div>
+      </li>
+
+
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+          <i class="mdi mdi-circle-outline menu-icon"></i>
           <span class="menu-title">Product</span>
           <i class="menu-arrow"></i>
         </a>
@@ -63,7 +78,7 @@
         </div>
       </li> --}}
       <li class="nav-item">
-        <a class="nav-link" href="">
+        <a class="nav-link" href="{{ route('admin.billings.index') }}">
           <i class="mdi mdi-view-headline menu-icon"></i>
           <span class="menu-title">Order</span>
         </a>
